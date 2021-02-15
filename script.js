e94d93d9c002f00aa2d10bbe79ca2ac1569f6ed1
saluja1 <amit.saluja.106@gmail.com>
2021-02-15 16:29:25 +0530

    rockPaperScissors_vanillaJS

---
+++ script.js
@@ -0,0 +1,65 @@
+let computerScore = 1;
+let playerScore = 1;
+const pScore = document.getElementById('playerScore');
+const cScore = document.getElementById('computerScore');
+const buttons = document.querySelectorAll('.selection button');
+const showIcon = document.querySelector('.show');
+const computerShowIcon = document.querySelector('.computer');
+
+const randomClasses = ["rock", "paper","scissors"];
+const text = document.getElementById('demo');
+const text2 = document.getElementById('demo2');
+
+
+const game = () =>{
+    buttons.forEach(btn =>{
+        btn.addEventListener('click',(e)=>{
+
+           let clickedBtn = e.target.className;
+           showIcon.className = clickedBtn;
+            $(showIcon).html(clickedBtn)
+           let randomNum = Math.floor(Math.random() * randomClasses.length);
+            $(computerShowIcon).html(randomClasses[randomNum]);
+            computerShowIcon.className = randomClasses[randomNum];
+
+           if(showIcon.className === computerShowIcon.className){
+               pScore.innerHTML = pScore.innerHTML;
+               cScore.innerHTML = cScore.innerHTML;
+               text.innerHTML = "It's a Tie ! ";
+               text.style.color = 'orange';
+           } // if it's not a Tie.
+           else if(showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[2]){
+               pScore.innerHTML = playerScore;
+               playerScore++;
+               text.innerHTML = "It's a Win ! ";
+               text.style.color = 'rgb(1, 146, 1)';
+           }else if(showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[1]){
+               cScore.innerHTML = computerScore;
+               computerScore++;
+               text.innerHTML = "You Loosed ! ";
+               text.style.color = 'red';
+           }else if(showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[2]){
+               cScore.innerHTML = computerScore;
+               computerScore++;
+               text.innerHTML = "You Loosed ! ";
+               text.style.color = 'red';
+           }else if(showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[0]){
+               pScore.innerHTML = playerScore;
+               playerScore++;
+               text.innerHTML = "It's a Win ! ";
+               text.style.color = 'rgb(1, 146, 1)';
+           }else if(showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[0]){
+               cScore.innerHTML = computerScore;
+               computerScore++;
+               text.innerHTML = "You Loosed ! ";
+               text.style.color = 'red';
+           }else if(showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[1]){
+               pScore.innerHTML = playerScore;
+               playerScore++;
+               text.innerHTML = "It's a Win ! ";
+               text.style.color = 'rgb(1, 146, 1)';
+           }
+        });
+    });
+}
+game();
